63f1bba2305db065ada91e33df444d161afe236f
shimo2383 <shimo2383@gmail.com>
2020-09-05 09:20:41 +0900

    新規登録/ログイン/ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date]
+    )
   end
-
-  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_054006) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false

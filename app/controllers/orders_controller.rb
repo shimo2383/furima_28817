d1c62ea9a2ef4ace3de7eb39fafcb6a104a6c9ee
shimo2383 <shimo2383@gmail.com>
2020-09-16 12:58:18 +0900

    商品購入機能 購入ページ条件123

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  #ログインしていないユーザーをログインページの画面に促すことができます。
 
   def index
-    if user_signed_in?
-      @item = Item.find(params[:item_id])
-    else
-      redirect_to user_session_path
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id  ||  @item.order != nil 
+      redirect_to root_path
     end
   end
 
   def create
   end
-  
+
 end
